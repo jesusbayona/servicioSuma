70ac63faa5619c3d7d5e0b3e5dfed79a0602af1f
jesusbayona <32921760+jesusbayona@users.noreply.github.com>
2017-10-22 20:12:13 -0500

    Pruebas
    Se realizan pruebas

--- Suma/start.sh
+++ Suma/start.sh
@@ -1,3 +1,3 @@
-docker pull jesusbayona/servicio
+docker pull jesusbayona/servicio:modval
 
-docker run -d --name servicio -p 8080:8080 jesusbayona/servicio
+docker run -d --name servicio -p 8080:8080 jesusbayona/servicio:modval
